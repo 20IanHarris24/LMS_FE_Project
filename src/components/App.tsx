3bc36905be6c2718ebd317765304706930a89f53
Ian <ian_harris@ymail.com>
2025-02-27 17:02:37 +0100

    admin change

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "../context/authProvider";
 import { router } from "../router";
 import { ApiDataProvider } from "../context/ApiDataProvider";
-import { AuthGuard } from "./AuthGuard";
+// import { AuthGuard } from "./AuthGuard";
 
 
 export function App() {
